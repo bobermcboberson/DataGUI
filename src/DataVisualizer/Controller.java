cb573227b3cd0422b86d376f6c5ffc639f82b0a5
Alonso L <tf2spyman5@gmail.com>
2021-04-19 14:31:05 -0400

    Edited .fxml and Controller.java

--- src/DataVisualizer/Controller.java
+++ src/DataVisualizer/Controller.java
@@ -1,10 +1,21 @@
 package DataVisualizer;
 
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class Controller {
     // Fields
-    public TableView BestSellingConsoleData;
+    public TableView BestSellingConsoleTableView;
     public TableView BestSellingGameTableView;
+    public TableColumn GamesRank;
+    public TableColumn GameName;
+    public TableColumn GameSold;
+    public TableColumn GamePlat;
+    public TableColumn GameDate;
+    public TableColumn GamePublish;
+    public TableColumn GameDev;
     public TableView BestSellingData;
+
+    // Methods
+
 }
