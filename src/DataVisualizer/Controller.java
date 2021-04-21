7c56fec2215cd18752209f28d292ba5a5d991d89
Alonso L <tf2spyman5@gmail.com>
2021-04-21 10:19:13 -0400

    Edited Controller.java to define TableColumn to BestSellingGames

--- src/DataVisualizer/Controller.java
+++ src/DataVisualizer/Controller.java
@@ -2,22 +2,23 @@ package DataVisualizer;
 
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import java.util.ArrayList;
 
 public class Controller {
     // Fields
     public TableView BestSellingConsoleTableView;
     public TableView BestSellingGameTableView;
-    public TableColumn GamesRank;
-    public TableColumn GameName;
-    public TableColumn GameSold;
-    public TableColumn GamePlat;
-    public TableColumn GameDate;
-    public TableColumn GamePublish;
-    public TableColumn GameDev;
+    public TableColumn<BestSellingGames, Integer> GamesRank;
+    public TableColumn<BestSellingGames, String> GameName;
+    public TableColumn<BestSellingGames, Integer> GameSold;
+    public TableColumn<BestSellingGames, String> GamePlat;
+    public TableColumn<BestSellingGames, String> GameDate;
+    public TableColumn<BestSellingGames, String> GamePublish;
+    public TableColumn<BestSellingGames, String> GameDev;
     public TableView BestSellingData;
 
     // Methods
-    public void initialize(){
+    public void initialize() {
         BestSellingConsole.read("BestSellingConsoleData");
         BestSelling.describeAll();
     }
