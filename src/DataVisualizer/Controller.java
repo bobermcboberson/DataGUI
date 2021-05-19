40d7cc076ed927fad59712c9b5fdacbb0439cf2e
Alonso L <tf2spyman5@gmail.com>
2021-05-19 10:37:10 -0400

    Program can now edit data in BestSellingGamesTableView cells.

--- src/DataVisualizer/Controller.java
+++ src/DataVisualizer/Controller.java
@@ -4,8 +4,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -39,19 +41,69 @@ public class Controller {
     // Methods
     public void initialize() {
         BestSellingGames.setMyController(this);
+        BestSellingGameTableView.setEditable(true);
 
-        GamesRank.setCellValueFactory(new PropertyValueFactory<>("ranking"));
-        GameName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        GameSold.setCellValueFactory(new PropertyValueFactory<>("unitSold"));
-        GamePlat.setCellValueFactory(new PropertyValueFactory<>("platform"));
-        GameDate.setCellValueFactory(new PropertyValueFactory<>("releaseDate"));
-        GamePublish.setCellValueFactory(new PropertyValueFactory<>("publisher"));
-        GameDev.setCellValueFactory(new PropertyValueFactory<>("creator"));
+        GamesRank.setCellValueFactory(new PropertyValueFactory("ranking"));
+        GamesRank.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        GamesRank.setOnEditCommit((GameRankEditEvent) -> {
+            Integer newValue = GameRankEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GameRankEditEvent.getRowValue();
+            editedRowObject.setRanking(newValue);
+        });
 
-        boolean hasData = gameRestore();
+        GameName.setCellValueFactory(new PropertyValueFactory("name"));
+        GameName.setCellFactory(TextFieldTableCell.forTableColumn());
+        GameName.setOnEditCommit((GameNameEditEvent) -> {
+            String newValue = GameNameEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GameNameEditEvent.getRowValue();
+            editedRowObject.setName(newValue);
+        });
+
+        GameSold.setCellValueFactory(new PropertyValueFactory("unitSold"));
+        GameSold.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        GameSold.setOnEditCommit((GameSoldEditEvent) -> {
+            Integer newValue = GameSoldEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GameSoldEditEvent.getRowValue();
+            editedRowObject.setUnitSold(newValue);
+        });
+
+        GamePlat.setCellValueFactory(new PropertyValueFactory("platform"));
+        GamePlat.setCellFactory(TextFieldTableCell.forTableColumn());
+        GamePlat.setOnEditCommit((GamePlatEditEvent) -> {
+            String newValue = GamePlatEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GamePlatEditEvent.getRowValue();
+            editedRowObject.setPlatform(newValue);
+        });
+
+        GameDate.setCellValueFactory(new PropertyValueFactory("releaseDate"));
+        GameDate.setCellFactory(TextFieldTableCell.forTableColumn());
+        GameDate.setOnEditCommit((GameDateEditEvent) -> {
+            String newValue = GameDateEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GameDateEditEvent.getRowValue();
+            editedRowObject.setReleaseDate(newValue);
+        });
+
+        GamePublish.setCellValueFactory(new PropertyValueFactory("publisher"));
+        GamePublish.setCellFactory(TextFieldTableCell.forTableColumn());
+        GamePublish.setOnEditCommit((GamePublishEditEvent) -> {
+            String newValue = GamePublishEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GamePublishEditEvent.getRowValue();
+            editedRowObject.setPublisher(newValue);
+        });
+
+        GameDev.setCellValueFactory(new PropertyValueFactory("creator"));
+        GameDev.setCellFactory(TextFieldTableCell.forTableColumn());
+        GameDev.setOnEditCommit((GameDevEditEvent) -> {
+            String newValue = GameDevEditEvent.getNewValue();
+            BestSellingGames editedRowObject = GameDevEditEvent.getRowValue();
+            editedRowObject.setCreator(newValue);
+        });
+
+        boolean hasData = this.gameRestore();
         if (hasData) {
-            updateGameDataUI();
+            this.updateGameDataUI();
         }
+
     }
 
     public boolean gameRestore() {
